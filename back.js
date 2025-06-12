4d6e3deb65c8fa2e9f66ca12862d14f10101578a
karthikdommeti <145791133+karthikdommeti@users.noreply.github.com>
2025-06-12 11:56:26 +0530

    Add files via upload

---
+++ back.js
@@ -0,0 +1,11 @@
+/* scroll reveal */
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay: 100,
+});
+
+ScrollReveal().reveal('.home-content, heading',{origin: 'top'});
+ScrollReveal().reveal('.home-img, .Projects-container, .Internship-box, .contact form',{origin: 'buttom'});
+ScrollReveal().reveal('.home-content p, .about-content',{origin: 'right'});
+ScrollReveal().reveal('.home-content, heading',{origin: 'top'});
